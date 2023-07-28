9dc8c133033680d2dafea2a794bdf1009f667530
Jason Hartsell (Astoundcommerce) <j.hartsell@astoundcommerce.com>
2023-07-28 12:15:43 -0700

    Update from Class based components to functional

---
+++ src/components/card/card.component.jsx
@@ -0,0 +1,31 @@
+// import { Component } from 'react';
+
+import './card.styles.css';
+
+const Card = ({monster}) => {
+    const {name, email, id } = monster;
+
+    return (
+        <div className='card-container' key={id}>
+            <img src={`https://robohash.org/${id}/?set=set2&size=180x180`} alt={`monster ${name}`} />
+            <h2>{name}</h2>
+            <p>{email}</p>
+        </div>
+    );
+};
+
+// class Card extends Component {
+//     render() {
+//         const {name, email, id } = this.props.monster;
+
+//         return (
+//             <div className='card-container' key={id}>
+//                 <img src={`https://robohash.org/${id}/?set=set2&size=180x180`} alt={`monster ${name}`} />
+//                 <h2>{name}</h2>
+//                 <p>{email}</p>
+//             </div>
+//         )
+//     }
+// }
+
+export default Card;
